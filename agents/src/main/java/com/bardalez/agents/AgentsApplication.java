6052438b0e79ee8b04fc005ebcd6aec4d87b8920
Cedric Bardalez <cedserh@gmail.com>
2026-09-12 23:11:29 -0500

    first commit - init project

---
+++ agents/src/main/java/com/bardalez/agents/AgentsApplication.java
@@ -0,0 +1,13 @@
+package com.bardalez.agents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentsApplication.class, args);
+	}
+
+}
